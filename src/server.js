70f43191d038640ccabfcc2f10fdca6189f60653
Romanna <romannabric@gmail.com>
2026-01-15 10:50:54 +0100

    add notes routes

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -16,19 +17,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(helmet());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
