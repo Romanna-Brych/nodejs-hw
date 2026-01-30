725af339e096743f65a70c826ba945625fc8db1a
Romanna <romannabric@gmail.com>
2026-01-30 13:46:59 +0100

    add logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
 } from '../validations/authValidation.js';
 import {
   loginUser,
+  logoutUser,
   refreshUserSession,
   registerUser,
 } from '../controllers/authController.js';
@@ -14,6 +15,7 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
 
 export default router;
