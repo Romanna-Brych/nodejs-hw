6cdadca9546b5d6754e2efdebcaab302d254dcc6
Romanna <romannabric@gmail.com>
2026-02-08 13:51:35 +0100

    +

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errors } from 'celebrate';
 
 const PORT = process.env.PORT ?? 3000;
@@ -23,6 +24,7 @@ app.use(helmet());
 
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
